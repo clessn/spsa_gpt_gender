dbe381ac03b19a415ae369a404d40ad9d0ad4c9e
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2023-11-08 20:39:53 -0500

    visible minority

--- code/test.prompt.R
+++ code/test.prompt.R
@@ -7,6 +7,7 @@ devtools::install_github("ellipse-science/sondr")
 #### Data laoding - Raw data CES 21 ####
 df_ces21 <- sondr::read_any_csv("_SharedFolder_spsa_gpt_gender/data/ces2021.csv")
 
+####
 
 #### OpenAi loop testing ####
 #### Test loop avec 1 seul répondant ####
