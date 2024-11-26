e5d7bb0ad92871e510641d75b82d046a5ca2046b
hubcad25 <hubertcadieux@gmail.com>
2024-11-26 17:05:47 -0500

    cramer general

--- code/cramer.R
+++ code/cramer.R
@@ -1,10 +1,14 @@
-df_ces21 <- read.csv("_SharedFolder_spsa_gpt_gender/data/CES21_CleanData_2024-01-07.csv")
+# Packages ---------------------------------------------------------------
+library(rcompanion)
+library(DescTools)
+library(dplyr)
 
+# Load data --------------------------------------------------------------
+df_ces21 <- read.csv("_SharedFolder_spsa_gpt_gender/data/CES21_CleanData_2024-01-07.csv")
 rawdata <- sondr::read_any_csv("_SharedFolder_spsa_gpt_gender/data/ces2021.csv")
 df_grille <- readRDS("_SharedFolder_spsa_gpt_gender/data/clean_ses_gpt.rds")
 
-### Cleaner les datas pour ce script
-
+# Clean data -------------------------------------------------------------
 table(df_ces21$female)
 df_ces21$gender <- NA
 df_ces21$gender[df_ces21$female == 1] <- "female"
@@ -60,42 +64,31 @@ df_ces21$rurality <- factor(df_ces21$rurality)
 table(df_ces21$rurality)
 
 visMin <- c("White", "Black", "Indigenous", "Asian", "Arab")
-table(rawdata$cps21_vismin_1)
-df_ces21$visMinArab <- NA
-df_ces21$visMinArab[rawdata$cps21_vismin_1 == 1] <- 1
-df_ces21$visMinArab[rawdata$cps21_vismin_1 != 1] <- 0
+
+df_ces21$visMinArab <- ifelse(rawdata$cps21_vismin_1 == 1, 1, 0)
 table(df_ces21$visMinArab)
+df_ces21$visMinAsian <- ifelse(rawdata$cps21_vismin_2 == 1, 1, 0)
+df_ces21$visMinBlack <- ifelse(rawdata$cps21_vismin_3 == 1, 1, 0)
+df_ces21$visMinIndigenous <- ifelse(rawdata$cps21_vismin_4 == 1, 1, 0)
+df_ces21$visMinWhite <- ifelse(rawdata$cps21_vismin_9 == 1, 1, 0)
 
-table(rawdata$cps21_vismin_2)
-df_ces21$visMinAsian <- NA
-df_ces21$visMinAsian[rawdata$cps21_vismin_2 == 1] <- 1
-df_ces21$visMinAsian[rawdata$cps21_vismin_2 != 1] <- 0
-table(df_ces21$visMinAsian)
+df_ces21$visMin <- factor(
+  dplyr::case_when(
+    rawdata$cps21_vismin_1 == 1 ~ "Arab",
+    rawdata$cps21_vismin_2 == 1 ~ "Asian",
+    rawdata$cps21_vismin_3 == 1 ~ "Black",
+    rawdata$cps21_vismin_4 == 1 ~ "Indigenous",
+    rawdata$cps21_vismin_9 == 1 ~ "White",
+    TRUE ~ "Other"
+  ),
+  levels = c("Arab", "Asian", "Black", "Indigenous", "White", "Other")
+)
 
-table(rawdata$cps21_vismin_3)
-df_ces21$visMinBlack <- NA
-df_ces21$visMinBlack[rawdata$cps21_vismin_3 == 1] <- 1
-df_ces21$visMinBlack[rawdata$cps21_vismin_3 != 1] <- 0
-table(df_ces21$visMinBlack)
+table(df_ces21$visMin)
 
-table(rawdata$cps21_vismin_4)
-df_ces21$visMinIndigenous <- NA
-df_ces21$visMinIndigenous[rawdata$cps21_vismin_4 == 1] <- 1
-df_ces21$visMinIndigenous[rawdata$cps21_vismin_4 != 1] <- 0
-table(df_ces21$visMinIndigenous)
+# Scales -----------------------------------------------------------------
 
-table(rawdata$cps21_vismin_9)
-df_ces21$visMinWhite <- NA
-df_ces21$visMinWhite[rawdata$cps21_vismin_9 == 1] <- 1
-df_ces21$visMinWhite[rawdata$cps21_vismin_9 != 1] <- 0
-table(df_ces21$visMinWhite)
-
-### Catégorie de référence
-
-df_ces21$gender <- relevel(df_ces21$gender, ref = "white_man")
-
-
-# Intervention de l'État --------------------------------------------------
+## Intervention de l'État --------------------------------------------------
 
 nnas_gd_econo <- (1 - is.na(df_ces21$issProbInegality21)) +
   (1 - is.na(df_ces21$issGovShouldDoStdOfLiving21)) +
@@ -109,7 +102,7 @@ df_ces21$scale_gd_econo[is.nan(df_ces21$scale_gd_econo)] <- NA
 
 hist(df_ces21$scale_gd_econo)
 
-# Environnement -----------------------------------------------------------
+## Environnement -----------------------------------------------------------
 
 nnas_enviro <-
   (1 - is.na(df_ces21$issTaxeCarbone21)) +
@@ -124,13 +117,12 @@ df_ces21$scale_enviro[is.nan(df_ces21$scale_enviro)] <- NA
 
 hist(df_ces21$scale_enviro)
 
-# Immigration -------------------------------------------------------------
+## Immigration -------------------------------------------------------------
 
 nnas_immigr <- (1 - is.na(df_ces21$issGovSpendImmigr21)) +
   (1 - is.na(df_ces21$issImmigrEnleveJobs21)) +
   (1 - is.na(df_ces21$issIntégrationImmigr21))
 table(nnas_immigr)
-
 
 df_ces21$scale_immigr <- (ifelse(is.na(df_ces21$issGovSpendImmigr21), 0, df_ces21$issGovSpendImmigr21) +
                          ifelse(is.na(df_ces21$issImmigrEnleveJobs21), 0, df_ces21$issImmigrEnleveJobs21) +
@@ -141,10 +133,40 @@ df_ces21$scale_immigr[is.nan(df_ces21$scale_immigr)] <- NA
 hist(df_ces21$scale_immigr)
 
 
-# Relevel -----------------------------------------------------------------
+# Scales in gpt data -----------------------------------------------------
 
-df_ces21$gender <- relevel(df_ces21$gender, ref = "male")
+df_grille$scale_gpt_intervention <- (df_grille$rich_gap_gpt + df_grille$ineq_prob_gpt + df_grille$option_priv_gpt) / 3
 
-# Model gauche-droite -----------------------------------------------------
+df_grille$scale_gpt_enviro <- (df_grille$reduce_emi_gpt + df_grille$cont_carb_tax_gpt + df_grille$enviro_reg_gpt) / 3
+
+df_grille$scale_gpt_immigr <- (df_grille$fed_spend_gpt + df_grille$recent_immi_gpt + df_grille$immi_take_jobs_gpt) / 3
+
+# Join gpt scales on df_ces21 --------------------------------------------
+
+df_join <- df_grille |> 
+  select(
+    gender, age, education,
+    income, province, rurality, visMin,
+    scale_gpt_enviro, scale_gpt_immigr, scale_gpt_intervention
+  ) %>%
+  left_join(
+    df_ces21, .,
+    by = c(
+      "gender", "age", "education",
+      "income", "province", "rurality",
+      "visMin"
+    )
+  ) |> 
+  tidyr::drop_na(
+    scale_gd_econo,
+    scale_enviro,
+    scale_immigr,
+    scale_gpt_enviro, scale_gpt_immigr, scale_gpt_intervention
+  )
 
 
+# Cramer's V -------------------------------------------------------------
+
+rcompanion::cramerV(df_join$scale_gd_econo, df_join$scale_gpt_intervention)
+rcompanion::cramerV(df_join$scale_enviro, df_join$scale_gpt_enviro)
+rcompanion::cramerV(df_join$scale_immigr, df_join$scale_gpt_immigr)
